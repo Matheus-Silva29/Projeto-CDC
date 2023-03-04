50de0386cb469b534f8109928bcf77303d38ed47
Matheus-Silva29 <cmatheusdeveloper@gmail.com>
2023-03-04 16:57:30 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// ABRIR MENU
+
+const btnMobile = document.querySelector(".btn-mobile");
+
+function toggleMenu() {
+  const nav = document.querySelector(".nav");
+  nav.classList.toggle("active");
+}
+
+btnMobile.addEventListener("click", toggleMenu);
